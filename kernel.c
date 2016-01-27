9dd04ca7d58d4e5e0cd99582ac868465375bc9a5
hgeel <hgeel@edu.tecnocampus.cat>
2016-01-27 23:23:29 +0100

    Changed some things. No saves the cursor position.

--- kernel.c
+++ kernel.c
@@ -1,36 +1,50 @@
+char* vidmem = (char*) 0xB8000;
+
+int currentPosX = 0;
+int currentPosY = 0;
+
+void set_cursor_pos(int x, int y)
+{
+        if(x < 0 || x >= 80 || y < 0 || y >= 25) return;
+        currentPosX = x;
+        currentPosY = y;
+}
+
 void put_char(char c, int x, int y)
 {
-	char* vidmem = (char*) 0xB8000;
 	int pos = y * 80 + x;
 	vidmem[pos*2] = c;
 	vidmem[pos*2+1] = 0x02;
 }
 
-void put_string_pos(const char* string, int x, int y)
+void put_string(const char* string)
 {
 	int i = 0;
 	while(string[i] != 0)
 	{
-		put_char(string[i], x, y);
-		x = x+1;
-		i = i+1;
-		if(x >= 80)
+		put_char(string[i], currentPosX, currentPosY);
+		i++;
+		currentPosX++;
+		if(currentPosX >= 80)
 		{
-			x = 0;
-			y = y+1;
-			if(y >= 25) y = 0;
+			currentPosX = 0;
+			currentPosY++;
+			if(currentPosY >= 25) currentPosY = 0;
 		}
 	}
 }
 
-void put_string(const char* string)
+void put_string_pos(const char* string, int x, int y)
 {
-	put_string_pos(string, 0, 0);
+	int prevX = currentPosX;
+	int prevY = currentPosY;
+	set_cursor_pos(x, y);
+	put_string(string);
+	set_cursor_pos(prevX, prevY);
 }
 
 void clear_screen()
 {
-	char* vidmem = (char*) 0xB8000;
 	int i = 0;
 	while(i < 80 * 25 * 2)
 	{
@@ -43,5 +57,9 @@ void clear_screen()
 void kernel_main(void)
 {
 	clear_screen();
-	put_string("Hello World Kernel!");
+	int i;
+	for(i = 0; i < 80; i++)
+	{
+		put_string("H");
+	}
 }
